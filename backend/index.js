6858a6fcae52a3b07f362e9e28a256a646fed640
tal-sharabi <145335298+tal-sharabi@users.noreply.github.com>
2024-08-04 18:56:21 +0300

    CORS policy and create React project,Vite,Tailwind CSS

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,32 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-import booksRoute from './routes/booksRoute.js'
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json());
 
+//Middleware for handling CORS POLICY
+//option 1 :Allow All Origins with Default of cors(*)
+//app.use(cors())
+//Option 2:Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("הצלחתי להקים שרת בחסדי ה שיר כפרוני עלייך");
 });
 
-app.use('/books',booksRoute);
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
