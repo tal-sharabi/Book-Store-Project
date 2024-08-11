22e3517029f0776c45cd3c025dbbcc05d8bafff3
tal-sharabi <145335298+tal-sharabi@users.noreply.github.com>
2024-08-11 20:29:01 +0300

    Show Book Deatails in React

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle} from 'react-icons/bs'
+import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 const Home = () => {
@@ -64,14 +64,14 @@ const Home = () => {
                 </td>
                 <td className="border border-slate-700 rounded-md text-center">
                   <div className="flex justify-center gap-x-4">
-                    <Link to={'/book/details/${book._id}'}>
-                      <BsInfoCircle className='text-2x1 text-green-800' />
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className="text-2x1 text-green-800" />
                     </Link>
-                    <Link to={'/book/edit/${book._id}'}>
-                      <AiOutlineEdit className='text-2x1 text-yellow-600' />
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2x1 text-yellow-600" />
                     </Link>
-                    <Link to={'/book/delete/${book._id}'}>
-                      <MdOutlineDelete className='text-2x1 text-red-600' />
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete className="text-2x1 text-red-600" />
                     </Link>
                   </div>
                   {book.publishYear}
